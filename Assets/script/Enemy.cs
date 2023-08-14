a6d47aef2c923e7a5450b00c322858de81135ffa
YunMiiiii <139723213+YunMiiiii@users.noreply.github.com>
2023-08-15 02:51:41 +0900

    attackScore 10점씩

--- Assets/script/Enemy.cs
+++ Assets/script/Enemy.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//점수
+
 public class Enemy : MonoBehaviour
 {
     public float speed = 1.0f;
@@ -9,34 +11,60 @@ public class Enemy : MonoBehaviour
     GameObject Player;
 
     int hp = 3;
-    // Start is called before the first frame update
+
+    GameManager gameManager;
+    
     void Start()
     {
-        
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+
     }
     private void OnCollisionEnter(Collision otherObject)
     {
         hp--;
+        //피격시 attackScore를 10씩 올린다.
+        gameManager.attackScore += 10;
+        gameManager.attackScoreTxt.text = gameManager.attackScore.ToString();
+
         if (otherObject.gameObject.tag == "Player")
         {
             Player.GetComponent<playerMove>().hp--;
-            if(Player.GetComponent<playerMove>().hp < 0)
+            if (Player.GetComponent<playerMove>().hp < 0)
             {
                 Destroy(otherObject.gameObject);
+
+                gameManager.bestScore = gameManager.attackScore + gameManager.destroyScore;
+                gameManager.bestScoreTxt.text = gameManager.bestScore.ToString();
+
+                //최고점수를 플렛폼 레지스트리에 저장
+                PlayerPrefs.SetInt("Best Score", gameManager.bestScore);
             }
-        
+            //피격시 destroyScore를 100씩 올린다.
+            gameManager.destroyScore += 100;
+            gameManager.destroyScoreTxt.text = gameManager.destroyScore.ToString();
+
+            Destroy(gameObject);
+        }
         else if (hp < 0)
-            {
-                Destroy(gameObject);
-                Destroy(otherObject.gameObject);
-            }
-            
+        {
+            Destroy(gameObject);
+            Destroy(otherObject.gameObject);
         }
     }
+
+    private void OnCollisionStay(Collision collision)
+    {
+
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+
+    }
+
 }
+
