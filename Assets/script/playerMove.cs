8762e12a3dd494c41f596a65e2621f85edd6c19f
YunMiiiii <139723213+YunMiiiii@users.noreply.github.com>
2023-08-16 17:13:48 +0900

    뭐야이게~

--- Assets/script/playerMove.cs
+++ Assets/script/playerMove.cs
@@ -7,11 +7,18 @@ public class playerMove : MonoBehaviour
 {
     public float speed = 5;
     public int hp = 5;
+
+    public VariableJoystick joystick;
     void Update()
     {
+
+#if UNITY_ANDROID
+float h = joystick.Horizontal;
+float v = joystick.Vertical;
+#elif UNITY_EDITOR || UNTIY_STANDALONE
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
-
+#endif
         Vector3 dir = Vector3.right * h + Vector3.up * v;
         transform.Translate(dir * speed * Time.deltaTime);
     }
